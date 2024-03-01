c064cd240235f8826feec89bd27a877f5e5d3cf3
chyuhung <977599667@qq.com>
2024-03-01 17:09:04 +0800

    用户登录验证

--- backend/controllers/v1/login.go
+++ backend/controllers/v1/login.go
@@ -21,18 +21,19 @@ func Login(c *gin.Context) {
 		// 生成JWT token
 		token, err := middlewares.SetToken(username)
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
+			c.JSON(http.StatusOK, gin.H{
 				"message": "生成token失败",
 			})
 		}
 		// 登录成功
 		c.JSON(http.StatusOK, gin.H{
+			"status":  200,
 			"message": "登录成功",
 			"token":   token,
 		})
 	} else {
 		// 登录失败
-		c.JSON(http.StatusUnauthorized, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"message": "用户名或密码错误",
 		})
 	}
